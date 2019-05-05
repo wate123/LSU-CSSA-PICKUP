37673980c806c9029f5af0b538021acc885ca641
wate123 <junlin0604@gmail.com>
2019-05-05 00:40:59 -0500

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'styled-components',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    [
+      'import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: 'css', // `style: true` 会加载 less 文件
+      },
+    ],
+  ],
+  env: {
+    production: {
+      only: ['app'],
+      plugins: [
+        'lodash',
+        'transform-react-remove-prop-types',
+        '@babel/plugin-transform-react-inline-elements',
+        '@babel/plugin-transform-react-constant-elements',
+      ],
+    },
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'dynamic-import-node',
+      ],
+    },
+  },
+};
